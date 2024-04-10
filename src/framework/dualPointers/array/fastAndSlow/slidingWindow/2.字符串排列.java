10be365fd7204b70ede813e65f1cf50557147e48
MarkTuwen <m18698577021@163.com>
2024-04-11 07:55:52 +0800

    滑动窗口：字符串的排列

---
+++ src/framework/dualPointers/array/fastAndSlow/slidingWindow/2.字符串排列.java
@@ -0,0 +1,83 @@
+package framework.dualPointers.array.fastAndSlow.slidingWindow;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author: Mr. Ye
+ * @description: 567. 字符串的排列
+ * @createDate: 2024/04/11
+ */
+
+class Solution {
+  /**
+   * 字符串的排列
+   * 
+   * @param s1 子串
+   * @param s2 目标字符串
+   * @return 是否存在 s1 的排列之一是 s2 的子串
+   */
+  public static boolean checkInclusion(String s1, String s2) {
+    // 初始化
+    int left = 0, right = 0, valid = 0;
+    Map<Character, Integer> need = new HashMap<>(5);
+    Map<Character, Integer> window = new HashMap<>(5);
+    // need赋值
+    for(char c : s1.toCharArray()) {
+      updateMap(need, c, 1);
+    }
+
+    // 窗口滑动
+    while (right < s2.length()) {
+      // 窗口增大
+      char cRight = s2.charAt(left);
+      right++;
+      // 更新数据
+      if (need.containsKey(cRight)) {
+        updateMap(window, cRight, 1);
+        if (window.get(cRight).equals(need.get(cRight))) {
+          valid++;
+        }
+      }
+
+      // 缩小窗口（维护定长窗口，此处只判断一次，可改为if）
+      while (right - left >= s1.length()) {
+        if (valid == need.size()) {
+          return true;
+        }
+        // 更新数据
+        char cLeft = s2.charAt(left);
+        left++;
+        if (need.containsKey(cLeft)) {
+          if (window.get(cLeft).equals(need.get(cLeft))) {
+            valid--;
+          }
+          updateMap(window, cLeft, -1);
+        }
+      }
+    }
+    return false;
+  }
+
+  /**
+   * 更新map的目标字符个数
+   * 
+   * @param map 目标map
+   * @param c 目标字符
+   * @param num 更新的字符个数
+   */
+  public static void updateMap(Map<Character, Integer> map, char c, int num) {
+    if (map.containsKey(c)) {
+      map.put(c, map.get(c) + num);
+    } else {
+      map.put(c, 1);
+    }
+  }
+
+  public static void main(String[] args) {
+    String s1 = "ab";
+    String s2 = "eidbaooo";
+
+    System.out.println(checkInclusion(s1, s2));
+  }
+}
